7e24d674a298bf2ba6563eaae67ce3b701e03b38
FishSev <ilya.sevrugin@gmail.com>
2022-11-13 20:14:55 +0600

    добавил обработку ошибок задания порта

--- main.go
+++ main.go
@@ -3,19 +3,15 @@ package main
 import (
 	"kolesa-upgrade-team/delivery-bot/config"
 	"kolesa-upgrade-team/delivery-bot/internal/app"
+	"kolesa-upgrade-team/delivery-bot/internal/server"
 	"log"
-	"os"
 )
 
 func main() {
 
 	config := &config.Config{}
 
-	for _, arg := range os.Args {
-		if arg[:5] == "port=" {
-			config.Port = arg[5:]
-		}
-	}
+	config.Port = server.SetPort()
 
 	if err := app.Run(config); err != nil {
 		log.Fatalf("%v", err)
